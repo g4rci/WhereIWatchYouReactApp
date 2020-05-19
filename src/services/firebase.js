d7e101ea640adf57f940d1bf2729af1f120987e2
M.A.Garcia <m.angel.gf04.com>
2020-05-19 20:37:36 +0200

    firebse.js process.env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,7 +3,7 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
-  apiKey: "AIzaSyDfof07N7eLQhcl0CoDZspLlDY0YXX-g6s",
+  apiKey: `${process.env.REACT_APP_apiKey}-g6s`,
   authDomain: "where-i-watch-you.firebaseapp.com",
   databaseURL: "https://where-i-watch-you.firebaseio.com/"
 };
