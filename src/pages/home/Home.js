13c559b686e471f66d60b9f7db1ffbf365b50ff0
M.A.Garcia <m.angel.gf04.com>
2020-05-20 13:06:33 +0200

    background home page done

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,21 +4,22 @@ import { Link } from 'react-router-dom';
 export default class Home extends Component {
   render() {
     return (
-        <div className="home">
-        
+      <div className="home">
+      <div className='_background'></div>
+        <div>
         <section>
-          <div className="jumbotron jumbotron-fluid py-5">
+          <div className="jumbotron-fluid py-5">
             <div className="container text-center py-5">
-              <h1 className="display-4">Welcome Where I Watch You</h1>
-              <p className="lead">A great place to find where you can watch your favorite movies</p>
+              <h1 className="transparent jumbotron display-4">Welcome To Where I Watch You</h1>
+              <p className="transparent jumbotron lead">A great place to find where you can watch your favorite movies</p>
               <div className="mt-4">
-                <Link className="btn btn-dark px-5 mr-3 mt-4" to="/signup">Create New Account</Link>
-                <Link className="btn btn-dark px-5 mr-3 mt-4" to="/login">Login to Your Account</Link>
+                <Link className="transparent btn btn-light px-5 mr-3 mt-4" to="/signup">Create New Account</Link>
+                <Link className="transparent btn btn-light px-5 mr-3 mt-4" to="/login">Login to Your Account</Link>
               </div>
             </div>
           </div>
         </section>
-        
+        </div>
       </div>
     )
   }
