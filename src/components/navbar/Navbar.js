36ad0b09fb905f90f1c9195f4ffcfe94d3d3d1eb
M.A.Garcia <m.angel.gf04.com>
2020-11-25 11:59:21 +0100

    logOut button moved from navBar to homePage

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,65 +1,92 @@
-import React from 'react'
-import { auth } from '../../services/firebase';
-import './NavBar.css'
-import { Nav, Form, FormControl, Button } from 'react-bootstrap'
-import Navbar from 'react-bootstrap/Navbar'
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { auth } from "../../services/firebase";
+import "./NavBar.css";
+import { Nav, Form, FormControl, Button } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import { withRouter } from "react-router-dom";
 
 class Navigation extends React.Component {
-  state = { name: ''};
-  
+  state = { name: "" };
+
   searchFilter = (search) => {
     this.setState({
       name: search.target.value,
-    }); 
+    });
   };
-  
-  submitForm1 (e) {
-    this.props.history.push(`/moviesearch/${this.state.name}`); 
+
+  submitForm1(e) {
+    this.props.history.push(`/moviesearch/${this.state.name}`);
   }
 
-  submitForm2 (e) {
-    this.props.history.push(`/showsearch/${this.state.name}`); 
+  submitForm2(e) {
+    this.props.history.push(`/showsearch/${this.state.name}`);
   }
-  
-render () {
-  
-  const { name } = this.state
+
+  render() {
+    const { name } = this.state;
     return (
-      <div className='navBar'>
+      <div className="navBar">
         <Navbar bg="dark" variant="dark" expand="lg">
           <Navbar.Brand href="/">Where I Watch You</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            {auth().currentUser ?
+            {auth().currentUser ? (
               <Nav className="mr-auto">
-              <Nav.Link href="/topmovies">Movies</Nav.Link>
-              <Nav.Link href="/topshows">TvShows</Nav.Link>
-              <Nav.Link href="/" onClick={() => auth().signOut()}>LogOut</Nav.Link>
+                <Nav.Link href="/topmovies">Movies</Nav.Link>
+                <Nav.Link href="/topshows">TvShows</Nav.Link>
               </Nav>
-              :
+            ) : (
               <Nav className="mr-auto">
-              <Nav.Link href="/login">LogIn</Nav.Link>
-              <Nav.Link href="/signup">SignUp</Nav.Link>
+                <Nav.Link href="/login">LogIn</Nav.Link>
+                <Nav.Link href="/signup">SignUp</Nav.Link>
               </Nav>
-            }
-            {this.props.location.pathname.includes('/topmovies') ||
-            this.props.location.pathname.includes('/moviesearch') ||
-            this.props.location.pathname.includes('/moviedetails') ?
-            <Form onSubmit={this.submitForm1.bind(this)} inline>
-              <FormControl href={`/moviesearch/${this.state.name}`} className='mb-2 mt-2 mr-sm-2' value={name} onChange={this.searchFilter} type="search" placeholder="Search Movie" />
-              <Button type='submit' className='mb-2 mt-2' href={`/moviesearch/${this.state.name}`} variant="outline-success" >Buscar</Button>
-            </Form>
-            :
-            <Form onSubmit={this.submitForm2.bind(this)} inline>
-              <FormControl href={`/showsearch/${this.state.name}`} className='mb-2 mt-2 mr-sm-2' value={name} onChange={this.searchFilter} type="search" placeholder="Search TvShow" />
-              <Button type='submit' className='mb-2 mt-2' href={`/showsearch/${this.state.name}`} variant="outline-success" >Buscar</Button>
-            </Form>
-            }
+            )}
+            {this.props.location.pathname.includes("/topMovies") ||
+            this.props.location.pathname.includes("/moviesearch") ||
+            this.props.location.pathname.includes("/moviedetails") ? (
+              <Form onSubmit={this.submitForm1.bind(this)} inline>
+                <FormControl
+                  href={`/moviesearch/${this.state.name}`}
+                  className="mb-2 mt-2 mr-sm-2"
+                  value={name}
+                  onChange={this.searchFilter}
+                  type="search"
+                  placeholder="Search Movie"
+                />
+                <Button
+                  type="submit"
+                  className="mb-2 mt-2"
+                  href={`/moviesearch/${this.state.name}`}
+                  variant="outline-success"
+                >
+                  Buscar
+                </Button>
+              </Form>
+            ) : (
+              <Form onSubmit={this.submitForm2.bind(this)} inline>
+                <FormControl
+                  href={`/showsearch/${this.state.name}`}
+                  className="mb-2 mt-2 mr-sm-2"
+                  value={name}
+                  onChange={this.searchFilter}
+                  type="search"
+                  placeholder="Search TvShow"
+                />
+                <Button
+                  type="submit"
+                  className="mb-2 mt-2"
+                  href={`/showsearch/${this.state.name}`}
+                  variant="outline-success"
+                >
+                  Buscar
+                </Button>
+              </Form>
+            )}
           </Navbar.Collapse>
         </Navbar>
       </div>
-    ) 
-    }}
+    );
+  }
+}
 
-    export default withRouter(Navigation)
+export default withRouter(Navigation);
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,26 +1,53 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { auth } from "../../services/firebase";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
-      <div className='_background'></div>
+        <div className="_background"></div>
         <div>
-        <section>
-          <div className="jumbotron-fluid py-5">
-            <div className="container text-center py-5">
-              <h1 className="transparent jumbotron display-4">Welcome To Where I Watch You</h1>
-              <p className="transparent jumbotron lead">A great place to find where you can watch your favorite movies</p>
-              <div className="mt-4">
-                <Link className="transparent btn btn-light px-5 mr-3 mt-4" to="/signup">Create New Account</Link>
-                <Link className="transparent btn btn-light px-5 mr-3 mt-4" to="/login">Login to Your Account</Link>
+          <section>
+            <div className="jumbotron-fluid py-5">
+              <div className="container text-center py-5">
+                <h1 className="transparent jumbotron display-4">
+                  Welcome To Where I Watch You
+                </h1>
+                <p className="transparent jumbotron lead">
+                  A great place to find where you can watch your favorite movies
+                </p>
+                {auth().currentUser ? (
+                  <div className="mt-4">
+                    <Link
+                      className="transparent btn btn-light px-5 mr-3 mt-4"
+                      to="/"
+                      onClick={() => auth().signOut()}
+                    >
+                      LogOut
+                    </Link>
+                  </div>
+                ) : (
+                  <div className="mt-4">
+                    <Link
+                      className="transparent btn btn-light px-5 mr-3 mt-4"
+                      to="/signup"
+                    >
+                      Create New Account
+                    </Link>
+                    <Link
+                      className="transparent btn btn-light px-5 mr-3 mt-4"
+                      to="/login"
+                    >
+                      Login to Your Account
+                    </Link>
+                  </div>
+                )}
               </div>
             </div>
-          </div>
-        </section>
+          </section>
         </div>
       </div>
-    )
+    );
   }
 }
